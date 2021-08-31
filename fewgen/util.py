7e5ebd5c07adc22a778fcb941cb9d0aacedad261
tabasy <m.tabasy@gmail.com>
2021-08-31 13:27:23 +0430

    Update util.py

--- fewgen/util.py
+++ fewgen/util.py
@@ -1,1 +1,11 @@
+import torch
 
+def all_to(inputs, device):
+  if torch.is_tensor(inputs):
+    return inputs.to(device)
+  if isinstance(inputs, tuple):
+    return tuple([all_to(i, device) for i in inputs])
+  if isinstance(inputs, list):
+    return [all_to(i, device) for i in inputs]
+  if isinstance(inputs, dict):
+    return {k:all_to(v, device) for k, v in inputs.items()}
